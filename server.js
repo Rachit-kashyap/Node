bbea6f9687301a2515748041b103ec2ef3e384f9
Rachit-kashyap <168331660+Rachit-kashyap@users.noreply.github.com>
2024-12-17 18:55:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const axios = require("axios");
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/",(req,res)=>{
+    let query = null;
+    res.render("index",{query});
+})
+
+app.post("/search", async (req, res) => {
+    const query = req.body.query;
+
+    try {
+        const _query = `https://lexica.art/api/v1/search?q=${encodeURIComponent(query)}`;
+        const response = await axios.get(_query); // Sending a GET request
+
+        const images = response.data.images; // Assuming the API returns images in a property named 'images'
+
+        // Redirect to the home page with the results
+        res.render("data", { images });
+    } catch (error) {
+        console.error("Error fetching data from Lexica API:", error.message);
+        res.render("data", { query: null, images: null });
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+app.get("/:query",(req,res)=>{
+    let err = req.params.query;
+    res.render("error_page",{err});
+})
